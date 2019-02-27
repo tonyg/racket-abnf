8b9415c89194c553a3120090707446165824de8b
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2019-02-27 05:52:06 +0000

    Expose more content in main.rkt

--- abnf/main.rkt
+++ abnf/main.rkt
@@ -3,8 +3,12 @@
 (provide ->parse-input
          parse-input?
          traverse
+         abnf-parser
          define-abnf-parser
-         flatten) ;; often useful for semantic functions
+         flatten ;; often useful for semantic functions
+         (struct-out exn:fail:abnf)
+         (struct-out exn:fail:abnf:syntax)
+         (struct-out exn:fail:abnf:ambiguity))
 
 (require "runtime.rkt")
 (require (only-in racket/list flatten))
