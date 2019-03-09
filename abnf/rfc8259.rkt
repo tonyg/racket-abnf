d7ad58fb111a3c0054d3bb06184988fbd27f12ac
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2019-03-09 12:20:14 +0000

    Avoid use of `traverse` in rfc8259.rkt

--- abnf/rfc8259.rkt
+++ abnf/rfc8259.rkt
@@ -9,24 +9,23 @@
 (require (only-in json json-null))
 
 (define (cst->ast cst)
-  (traverse
-   (lambda (walk cst)
-     (match cst
-       [`(JSON-text (: ,_ ,v ,_)) (walk v)]
-       [`(value ,v) (walk v)]
-       [`(array (: (begin-array ,_) (* ()) (end-array ,_))) '()]
-       [`(array (: (begin-array ,_) (* ((: ,v0 (* ((: ,_ ,vs) ...))))) (end-array ,_)))
-        (map walk (cons v0 vs))]
-       [`(object (: (begin-object ,_) (* ()) (end-object ,_))) (hasheq)]
-       [`(object (: (begin-object ,_) (* ((: ,v0 (* ((: ,_ ,vs) ...))))) (end-object ,_)))
-        (make-immutable-hasheq (map walk (cons v0 vs)))]
-       [`(member (: ,k ,_ ,v)) (cons (string->symbol (walk k)) (walk v))]
-       [`(string (: ,_q1 (* (,@cs)) ,_q2)) (unescape-json-string cs)]
-       [`(number ,n) (string->number (text n))]
-       [`(true ,_) #t]
-       [`(false ,_) #f]
-       [`(null ,_) (json-null)]))
-   cst))
+  (define (walk cst)
+    (match cst
+      [`(JSON-text (: ,_ ,v ,_)) (walk v)]
+      [`(value (/ ,_ ,v)) (walk v)]
+      [`(array (: (begin-array ,_) (* ()) (end-array ,_))) '()]
+      [`(array (: (begin-array ,_) (* ((: ,v0 (* ((: ,_ ,vs) ...))))) (end-array ,_)))
+       (map walk (cons v0 vs))]
+      [`(object (: (begin-object ,_) (* ()) (end-object ,_))) (hasheq)]
+      [`(object (: (begin-object ,_) (* ((: ,v0 (* ((: ,_ ,vs) ...))))) (end-object ,_)))
+       (make-immutable-hasheq (map walk (cons v0 vs)))]
+      [`(member (: ,k ,_ ,v)) (cons (string->symbol (walk k)) (walk v))]
+      [`(string (: ,_q1 (* (,@cs)) ,_q2)) (unescape-json-string cs)]
+      [`(number ,n) (string->number (text n))]
+      [`(true ,_) #t]
+      [`(false ,_) #f]
+      [`(null ,_) (json-null)]))
+  (walk cst))
 
 (define (leading-codepoint? c) (<= #xd800 c #xdbff))
 (define (trailing-codepoint? c) (<= #xdc00 c #xdfff))
